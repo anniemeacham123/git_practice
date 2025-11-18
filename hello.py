059563b0aa18f0073539ad2cd2bb7f9e14702f2a
anniemeacham123 <annie.meacham1@gmail.com>
2025-11-18 08:36:55 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-print("Hello, Git!")
+print("Greetings from Github!")
 print("In vi --> r is replace, i is insert, esc is to escape and the :wq is for write and quit")
+print("this version was edited on the web :0")
 
