dc3748e9fcbce9e9703d57dc4b8de9a93b6fdefb
Annie Meacham <annie.meacham1@gmail.com>
2025-11-18 10:26:03 -0700

    Add square function to calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,10 @@
 def add(a, b):
   return a + b
-
+def square(a)
+  """Return the square number"""
+  return a ** 2
 # Updated from the Github web interfaces
+if __name__ == "__main__":
+  print("Calculator Loaded!")
+  print(f"5 + 3 = {add(5, 3)}")
+  print(f"5 squared = {square(5)}")
